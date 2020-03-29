cfab0ad42f7d2a689b2eeb828080299b515c9a89
Syps <nsypteras@gmail.com>
2020-03-29 12:50:49 -0400

    change to turn hue lights on/off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from machine import Pin
 import urequests
+import json
 import time
 
 def do_connect():
@@ -20,6 +21,41 @@ def send_slack_alert():
     urequests.request("POST", url, data=payload)
 
 
+def any_lights_on(url):
+    data = urequests.request("GET", url)
+    lights = json.loads(data.text)
+    data.close() # important to close connection
+
+    for name, state in lights.items():
+      if state['state']['on']:
+        return True
+
+    return False
+
+
+def set_lights(url, on):
+    data = urequests.request("GET", url)
+    lights = json.loads(data.text)
+    data.close()
+
+    if on:
+      data='{"on":true, "bri":254}'
+    else:
+      data='{"on":false}'
+
+    for name in lights.keys():
+      r = urequests.request("PUT", url + "/" + name + "/state", data=data)
+      r.close()
+      
+
+def toggle_lights():
+    url = ''
+
+    if any_lights_on(url):
+      set_lights(url, False)
+    else:
+      set_lights(url, True)
+
 do_connect()
 
 btn_pressed = False
@@ -27,7 +63,7 @@ btn = Pin(14, Pin.IN, Pin.PULL_UP)
 while True:
     if not btn.value() and not btn_pressed:
         btn_pressed = True
-        send_slack_alert()
+        toggle_lights()
         time.sleep(.2)
     elif btn.value() and btn_pressed:
         btn_pressed = False
